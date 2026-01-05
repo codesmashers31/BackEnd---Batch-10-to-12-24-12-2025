6a57eb8be8207668cfe7372d9c6c6c52a52932ce
Bala <balasudahn17@gmail.com>
2026-01-05 12:48:39 +0530

    updated

---
+++ JS/import_export/app.js
@@ -0,0 +1,38 @@
+// import add,{muilt,sub} from './math.js'
+
+// const datas = "This is React"
+// console.log(add(2,3));
+// console.log(muilt(3,4));
+// console.log(sub(3,4));
+
+
+// console.log(`Enter the name ${datas} 
+//     fsdjvhksdjh
+//     sdgjhsdkjsdf
+//     sjdhvjsdsdf
+//     sdkfjhsdkjfh
+//     ${add(2,3)}
+//     `);
+
+
+
+
+
+// const arr = [1,2,3,34,4,5]
+
+// const [a,b,c,d] = arr
+// console.log(a, b, c, d);
+
+
+
+const obj = {name:"New",subject:"JS"}
+
+const {name,subject} = obj
+
+console.log(name, subject);
+
+console.log("newdata");
+
+
+
+// condition?truevalue:falsevalue
---
+++ JS/import_export/math.js
@@ -0,0 +1,44 @@
+const add = (a,b) =>{
+
+    return a+b;
+
+}
+
+export default add;
+
+export const muilt = (a=3,b)=>{
+
+    return a*b
+
+
+
+
+}
+
+
+export const sub = (a,b)=>{
+   return a-b
+}
+
+
+const name = "React"
+
+const user = {name:name}
+
+console.log(user);
+
+
+const newname = "Node"
+const datas = {name:"react",subject:"new"}
+
+const users = {datas}
+
+console.log(users);
+
+
+// ?.
+
+const optionaluser = {}
+console.log(optionaluser.name);
+
+console.log('codes')
