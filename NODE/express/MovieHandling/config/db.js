faf37225958bc41ed5930d68cc19e5bceabb3fa2
bala <balasudhan17@gmail.com>
2026-02-20 12:07:33 +0530

    updated

---
+++ NODE/express/MovieHandling/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async()=>{
+
+try {
+
+  const conn = await mongoose.connect(process.env.MONGO_URI)
+  console.log(`MongoDB Connected ${conn.connection.host}`);
+    
+} catch (error) {
+
+    console.log('Something error',error);
+    
+    
+}
+  
+
+}
+
+export default connectDB;
---
+++ NODE/express/MovieHandling/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './config/db.js'
+import movieRoute from './routes/movieRoute.js'
+
+dotenv.config()
+
+connectDB()
+const MovieApp = express()
+
+
+MovieApp.use(cors())
+MovieApp.use(express.json())
+
+MovieApp.use("/api/movie",movieRoute)
+
+const PORT = process.env.PORT || 3000
+
+
+MovieApp.listen(PORT,()=>{
+    console.log(`Server Connected Successfully http://localhost:${PORT}`);
+})
+
+
+// http://localhost:5000/api/movie
