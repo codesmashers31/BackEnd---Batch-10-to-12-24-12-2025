b686c1dac5460af8b8b70b62ca41e6d8f80cf78b
bala <balasudhan17@gmail.com>
2026-02-24 11:26:21 +0530

    updated

--- NODE/express/departmentdatas/routes/department_Route.js
+++ NODE/express/departmentdatas/routes/department_Route.js
@@ -12,3 +12,8 @@ route.put("/updateData/:userid",updatedata)
 route.delete("/deleteData/:userid",deletedata)
 
 export default route;
+
+// http://localhost:5000/api/department/add
+// // http://localhost:5000/api/department/get
+
+//  http://localhost:5000/api/department/deleteData/
