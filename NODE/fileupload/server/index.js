645bbae9516dc7930167989680f9a6b77c48c838
bala <balasudhan17@gmail.com>
2026-03-05 12:18:32 +0530

    updated

---
+++ NODE/fileupload/server/index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './config/db.js'
+import crudRoute from './routes/crudRoute.js'
+
+
+dotenv.config()
+
+
+const app = express()
+
+app.use(cors())
+ 
+app.use(express.json())
+
+app.use("/api/crud",crudRoute)
+
+app.use("/uploads",express.static("uploads"))
+
+
+connectDB()
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT,()=>{
+    console.log(`Server Conneceted Succfully ${PORT}`);
+    
+})
+
+
+
+// http://localhost:5000/api/auth
